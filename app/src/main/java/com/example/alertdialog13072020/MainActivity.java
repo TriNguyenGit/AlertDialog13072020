5d2dac5d0743bf43f7bf74e99fb6e43a8be4bd08
TriNguyenGit <nguyenvantri51480153@gmail.com>
2020-08-05 19:26:32 +0700

    SingleChoiceItems (Quan trong)

--- app/src/main/java/com/example/alertdialog13072020/MainActivity.java
+++ app/src/main/java/com/example/alertdialog13072020/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,26 +22,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 AlertDialog.Builder buider = new AlertDialog.Builder(MainActivity.this);
-                buider.setTitle("Thông báo");
-                buider.setMessage("Bạn có muốn thoát ứng dụng");
+                buider.setTitle("Chọn 1 trong các con vật");
                 buider.setIcon(R.mipmap.ic_launcher);
                 buider.setCancelable(false);//ngăn người dùng muốn thoát
+                final String[] arrayAnimals = {"Mèo","Cá","Chim","Heo","Gà","Hổ"};
+                //lựa chọn
+                buider.setSingleChoiceItems(arrayAnimals, -1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this,arrayAnimals[i], Toast.LENGTH_SHORT).show();
+                    }
+                });
                 //nút tích cực (Thoát ứng dụng)
                 buider.setPositiveButton("Có", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        finish();
-                    }
-                });
-                //nút tiêu cực (Không thoát)
-                buider.setNegativeButton("Không", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-                });
-                //nút hủy
-                buider.setNeutralButton("Hủy", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
 
